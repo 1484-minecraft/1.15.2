3ae68b86106699b348b79bc3fa67f19f9227b39b
1484-minecraft <angelus2003611@gmail.com>
2020-04-13 17:35:10 +0800

    'update'

--- stop.bat
+++ stop.bat
@@ -26,24 +26,26 @@ SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:108 /lparam:1
 :: ENTER
 SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
 
-rem ::Sending shutdown command
-rem echo Sending shutdown command to server: %server%
-rem ::s
-rem SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:115 /lparam:1
-rem ::t
-rem SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:116 /lparam:1
-rem ::o
-rem SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:111 /lparam:1
-rem ::p
-rem SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:112 /lparam:1
-rem :: ENTER
-rem SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
+::Sending shutdown command
+echo Sending shutdown command to server: %server%
+::s
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:115 /lparam:1
+::t
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:116 /lparam:1
+::o
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:111 /lparam:1
+::p
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:112 /lparam:1
+:: ENTER
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
 
 :loop
-FOR /F "tokens=*" %%a in ('tasklist /fi "WINDOWTITLE eq minecraft"') do SET OUTPUT=%%a
-echo %OUTPUT%
+For /f "Delims=:" %%A in ('tasklist /fi "WINDOWTITLE eq minecraft"') do SET OUTPUT=%%A
+::FOR /F "tokens=*" %%a in ('tasklist /fi "WINDOWTITLE eq minecraft"') do SET OUTPUT=%%a
 @ping 127.0.0.1 -n 2 -w 500 > nul
 if not "%OUTPUT:java=%" == "%OUTPUT%" goto loop
+
+echo Done
 
 git config user.name 1484-minecraft
 git config user.email angelus2003611@gmail.com
