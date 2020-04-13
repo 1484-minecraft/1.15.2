d0e4602a94bea3385a27bbd5a2cce8b401a16529
tom1484 <angelus0918866416@gmail.com>
2020-04-13 14:17:34 +0800

    'update'

--- stop.bat
+++ stop.bat
@@ -6,15 +6,15 @@ set server="minecraft"
 echo Sending shutdown command to server: %server%
 
 ::s
-SendMessage-1.1.2.exe /windowtitle:%server% /message:WM_CHAR /wparam:115 /lparam:1
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:115 /lparam:1
 ::t
-SendMessage-1.1.2.exe /windowtitle:%server% /message:WM_CHAR /wparam:116 /lparam:1
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:116 /lparam:1
 ::o
-SendMessage-1.1.2.exe /windowtitle:%server% /message:WM_CHAR /wparam:111 /lparam:1
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:111 /lparam:1
 ::p
-SendMessage-1.1.2.exe /windowtitle:%server% /message:WM_CHAR /wparam:113 /lparam:1
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:113 /lparam:1
 :: ENTER
-SendMessage-1.1.2.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
 
 git add --all
 git commit -m 'update'
