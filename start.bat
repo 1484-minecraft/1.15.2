8d03ae974d093702853786dd5fd6faa985d7800d
tom1484 <angelus0918866416@gmail.com>
2020-04-13 16:06:17 +0800

    'update'

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
 @echo off
 
+git config user.name 1484-minecraft
+git config user.email angelus2003611@gmail.com
 git pull https://1484-minecraft:staythefuckathome@github.com/1484-minecraft/1.15.2
 
 start "minecraft" java -Xms1572M -Xmx2048M -jar spigot.jar
