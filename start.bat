7f76c00b11d8ef0877d6fb3c46143398815c70b8
1484-minecraft <angelus2003611@gmail.com>
2020-04-13 17:20:56 +0800

    'update'

--- start.bat
+++ start.bat
@@ -2,5 +2,3 @@
 
 git pull https://1484-minecraft:staythefuckathome@github.com/1484-minecraft/1.15.2
 start "minecraft" java -Xms1572M -Xmx2048M -jar spigot.jar
-
-PAUSE
--- stop.bat
+++ stop.bat
@@ -2,19 +2,31 @@
 
 set server="minecraft"
 
-::Sending shutdown command
-echo Sending shutdown command to server: %server%
-
-::s
-SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:115 /lparam:1
-::t
-SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:116 /lparam:1
-::o
-SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:111 /lparam:1
-::p
-SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:112 /lparam:1
 :: ENTER
 SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
+
+::Sending shutdown command
+echo Sending save-all command to server: %server%
+::s
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:115 /lparam:1
+::a
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:97 /lparam:1
+::v
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:118 /lparam:1
+::e
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:101 /lparam:1
+::-
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:45 /lparam:1
+::a
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:97 /lparam:1
+::l
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:108 /lparam:1
+::l
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:108 /lparam:1
+:: ENTER
+SendMessage.exe /windowtitle:%server% /message:WM_CHAR /wparam:13 /lparam:1
+
+
 
 :loop
 FOR /F "tokens=*" %%a in ('tasklist /fi "WINDOWTITLE eq minecraft"') do SET OUTPUT=%%a
@@ -25,6 +37,6 @@ git config user.name 1484-minecraft
 git config user.email angelus2003611@gmail.com
 git add --all
 git commit -m 'update'
-git pull https://1484-minecraft:staythefuckathome@github.com/1484-minecraft/1.15.2
+git push https://1484-minecraft:staythefuckathome@github.com/1484-minecraft/1.15.2
 
 PAUSE
